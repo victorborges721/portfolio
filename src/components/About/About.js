69ca85cbbf7155d3eec98623bfb18d488210ab6f
Victor Borges <victor.borges.721@gmail.com>
2021-08-28 10:09:03 -0400

    Added photo to About section.

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Container } from "react-bootstrap";
+import { Container, Image, Row, Col } from "react-bootstrap";
+import borges_pic from "../../images/borges_pic.jpeg";
 import "./style.css";
 
 const About = () => {
@@ -10,6 +11,21 @@ const About = () => {
     >
       <Container>
         <h1>&#60;About&#62;</h1>
+        <Row>
+          <Col md={4}>
+            <div>
+              <Image
+                src={borges_pic}
+                alt="Image of Victor Borges"
+                roundedCircle
+                fluid
+              />
+            </div>
+          </Col>
+          <Col md={8}>
+            <h1>About Victor Borges</h1>
+          </Col>
+        </Row>
         <h1>&#60;/About&#62;</h1>
       </Container>
     </section>
